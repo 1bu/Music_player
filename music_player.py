a649e402d81418259c7240d8f6298caef6da44e6
Ibu <ivancarpio50@gmail.com>
2026-07-31 00:48:41 -0400

    Basic funcionalities

--- music_player.py
+++ music_player.py
@@ -1,5 +1,7 @@
 import tkinter as tk
+from tkinter import filedialog
 import os
+import subprocess
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
 import pygame
 
@@ -20,22 +22,19 @@ def main():
 
     if not os.path.isdir(MUSIC_DIR):
         os.mkdir(MUSIC_DIR)
-        print('Creating a new folder')
-
 
     for files in os.listdir(MUSIC_DIR):
         if files.lower().endswith(formats):
             songs.append(files)
 
-    UI(songs)
+    UI(songs, MUSIC_DIR)
     return 0
 
-def get_song_path(song_name):
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(BASE_DIR, 'Music', song_name)
+def get_song_path(song_name,state):
+    return os.path.join(state['current_directory'], song_name)
 
 def play_song(state,song_name,current_song,btn_action):
-    song_path = get_song_path(song_name)
+    song_path = get_song_path(song_name,state)
     current_song.set(song_name)
     pygame.mixer.music.load(song_path)
     pygame.mixer.music.play()
@@ -47,11 +46,19 @@ def play_song(state,song_name,current_song,btn_action):
 
 #Funcionalidades
 def toggle_play(btn_action, state, playlist,current_song):
-    selected_file = playlist.get(tk.ACTIVE)
+    selected_file = playlist.curselection()
+
+    if not selected_file and state['current_playing'] is None:
+        return
+    
+    if selected_file:
+        selection = playlist.get(selected_file[0])
+    else:
+        selection = state['current_file']
 
     #Si la cancion seleccionada es diferente
-    if state["current_file"] != selected_file:
-        play_song(state, selected_file, current_song,btn_action)
+    if state["current_file"] != selection:
+        play_song(state, selection, current_song,btn_action)
 
     #Si la cancion seleccionada es la misma -> se pause
     elif state['playing'] and not state['paused'] :
@@ -70,7 +77,7 @@ def stop_music(state):
         pygame.mixer.music.stop()
         state['playing'] = False
         state['paused'] = False
-        state['current_file']: None
+        state['current_file'] =  None
         
 def next_song(state, playlist,current_song,btn_action):
     if state['playing']:
@@ -104,11 +111,31 @@ def prev_song(state, playlist, current_song,btn_action):
         prev_song_name = playlist.get(prev_selected)
         play_song(state,prev_song_name, current_song, btn_action)
 
-def UI(files):
+def open_folder(base_dir,state):
+    try:   
+        return os.startfile(state['current_directory'])
+    except:
+        return subprocess.Popen(['xdg-open',state['current_directory']])
+
+def change_folder(playlist,state):
+    new_drt = (filedialog.askdirectory(title='Open a songs directory'))
+    
+    if new_drt:
+        state['current_directory'] = new_drt
+
+        playlist.delete(0, tk.END)
+        formats = ".mp3"
+        for file in os.listdir(new_drt):
+            if file.lower().endswith(formats):
+                playlist.insert(tk.END,file)
+
+
+def UI(files,base_dir):
     state = {
         "playing": False,
         "paused": False,
-        "current_file":None
+        "current_file":None,
+        "current_directory": base_dir
         }    
 
     #Canvas
@@ -159,6 +186,13 @@ def UI(files):
 
     prev_song_btn = tk.Button(btn_frame, text='<-', bg= 'Aqua', width = 8, command=lambda:prev_song(state,playlist,current_song,btn_action))
     prev_song_btn.place(x = 15, y = 10)
+
+    #Manejador de directorio
+    open_folder_btn = tk.Button(btn_frame, text='Open folder', bg='Aqua', width = 18, command=lambda:open_folder(base_dir,state))
+    open_folder_btn.place(x=20, y = 60)
+
+    change_folder_btn = tk.Button(btn_frame, text='Change folder', bg='Aqua', width = 18,command=lambda:change_folder(playlist,state))
+    change_folder_btn.place(x=205, y = 60)
 
     canvas.mainloop()
 
